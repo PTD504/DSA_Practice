387437f49020137d800b912085d14f27750a9771
PTD504 <22520193@gm.uit.edu.vn>
2024-07-16 10:11:40 +0700

    More Practice

---
+++ DSA_Sheet_Love_Babbar/Matrix/MaxRectangle.cpp
@@ -0,0 +1,86 @@
+﻿/*
+###########################################
+Source: GeeksforGeeks
+
+Problem statement:
+Given a binary matrix M of size n X m. Find the maximum area of a rectangle formed only of 1s in the given matrix.
+
+Constraints:
+1<=n,m<=1000
+0<=M[i][j]<=1
+
+Solution: PTD504
+###########################################
+*/
+
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <stack>
+
+#define max(a, b) (a > b ? a : b)
+
+const int MAX = 11;
+
+class Solution{
+public:
+
+    int maxRecInHistogram(int hist[], int n) {
+        std::stack<int> s;
+        int ans = 0, i = 0;
+
+        while (i < n) {
+            while (!s.empty() and hist[s.top()] > hist[i]) {
+                int t = s.top();
+                s.pop();
+                ans = max(ans, hist[t] * (s.empty() ? i : i - s.top() - 1));
+            }
+            s.push(i++);
+        }
+
+        while (!s.empty()) {
+            int t = s.top();
+            s.pop();
+
+            ans = max(ans, hist[t] * (s.empty() ? n : n - s.top() - 1));
+        }
+
+        return ans;
+    }
+
+    int maxArea(int M[][MAX], int n, int m) {
+        int hist[m], ans = 0;
+        memset(hist, 0, sizeof(hist));
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (M[i][j]) hist[j]++;
+                else hist[j] = 0;
+            }
+
+            ans = max(ans, maxRecInHistogram(hist, m));
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+    int r = 10, c = 11;
+    int matrix[][MAX] = {
+        {0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1},
+        {1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0},
+        {0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0},
+        {1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0},
+        {0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0},
+        {1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1},
+        {0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0},
+        {1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1},
+        {0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1},
+        {1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0}
+    };
+
+    Solution obj;
+
+    std::cout << obj.maxArea(matrix, r, c);
+}
---
+++ DSA_Sheet_Love_Babbar/Matrix/RowWithMax1s.cpp
@@ -0,0 +1,50 @@
+﻿/*
+###########################################
+Source: GeeksforGeeks
+
+Problem statement:
+Given a boolean 2D array, consisting of only 1's and 0's, where each row is sorted. Find the 0-based index of the first row that has the maximum number of 1's. Return the 0-based index of the first row that has the most number of 1s. If no such row exists, return -1.
+
+Constraints:
+1 ≤ number of rows, number of columns ≤ 10^3
+0 ≤ arr[i][j] ≤ 1 
+
+Solution: PTD504
+###########################################
+*/
+
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    int rowWithMax1s(std::vector<std::vector<int> > arr) {
+        int n = arr.size(), m = arr[0].size();
+        int i = 0, j = m - 1;
+        int ans = -1;
+
+        while (i < n and j >= 0) {
+            if (arr[i][j]) j--, ans = i;
+            else i++;
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+    int r = 7, c = 8;
+    std::vector<std::vector<int> > matrix = {
+        {0, 0, 0, 0, 0, 0, 0, 1},
+        {0, 0, 0, 0, 0, 1, 1, 1},
+        {0, 0, 0, 1, 1, 1, 1, 1},
+        {0, 0, 1, 1, 1, 1, 1, 1},
+        {0, 0, 0, 0, 1, 1, 1, 1},
+        {0, 0, 0, 1, 1, 1, 1, 1},
+        {0, 0, 1, 1, 1, 1, 1, 1}
+    };
+
+    Solution obj;
+
+    std::cout << obj.rowWithMax1s(matrix);
+}
