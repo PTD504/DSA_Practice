7278dca10fc915f98f36674ad490482a089b1413
PTD504 <22520193@gm.uit.edu.vn>
2025-05-03 21:10:36 +0700

    Sorting and Searching problems

---
+++ Top-Interview-Questions-Leetcode/Easy-Collections/sorting-and-search/first-bad-version.py
@@ -0,0 +1,16 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        l = 1
+        
+        while l <= n:
+            m = l + (n - l) // 2
+            
+            if isBadVersion(m):
+                n = m - 1
+            else:
+                l = m + 1
+        
+        return l
